8f80dda5ceba1295658a2066fd4853f90da52525
Stas1k <102374219+Stasik14@users.noreply.github.com>
2022-03-27 18:57:46 +0300

    Update Main.cpp

--- GitHub/Main.cpp
+++ GitHub/Main.cpp
@@ -2,39 +2,29 @@
 
 using namespace std;
 
-int main() {
-	int a, b, c, d;
+int main() 
+{
+	int a, b;
 	cout << "Input number a: ";
 	cin >> a;
 
 	cout << "Input number b: ";
 	cin >> b;
 
-	cout << "Input number c: ";
-	cin >> c;
-
-	cout << "Input number d: ";
-	cin >> d;
-
-
 	int max = 0;
 
-	if (a > b && a > c && a > d) {
+	if (a > b && a > c && a > d) 
+	{
 		max = a;
-	}else if(b > a && b > c && b > d) {
-		max = b;
-	}else if (c > a && c > b && c > d) {
-		max = c;
 	}
-	else {
-		max = d;
+	
+	else 
+	{
+		max = и;
 	}
 
 	cout << max;
-
 	
-
-
 	return 0;
 }
 
